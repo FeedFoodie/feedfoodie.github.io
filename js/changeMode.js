58272ccb2e6d7e37b216d7e04abdb9b0d8eba8b4
FeedFoodie <98862343+FeedFoodie@users.noreply.github.com>
2022-02-10 21:42:12 -0800

    font size

--- js/changeMode.js
+++ js/changeMode.js
@@ -1,5 +1,5 @@
 // Change Font Size
-var cont = document.getElementById("container");  
+var cont = document.getElementById("content");  
 function changeFontSize(change) {  
 	if (change == 0) {
 		cont.style.fontSize = 16px;
