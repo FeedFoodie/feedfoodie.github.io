decf523fbf533966b1cb30829c33aba0004fc193
FeedFoodie <98862343+FeedFoodie@users.noreply.github.com>
2022-02-14 14:23:27 -0800

    Update changeMode.js

--- js/changeMode.js
+++ js/changeMode.js
@@ -3,10 +3,18 @@ var currentSize = 0;
 function changeFontSize(change) {  
 	if (change == '0') {
 		document.getElementById("content").style.fontSize = "16px";
+		var quote = document.getElementsByClassName("night-mode-quotes");
+		for (var i = 0; i < quote.length; i++) {
+		  quote[i].style.fontSize = "16px";
+		}
 	}
      else {
 		currentSize = parseInt(document.getElementById("content").style.fontSize);
         document.getElementById("content").style.fontSize = currentSize + change + 'px';
+		var quote = document.getElementsByClassName("night-mode-quotes");
+		for (var i = 0; i < quote.length; i++) {
+		  quote[i].style.fontSize = currentSize + change + 'px';
+		}
 	}	
 }
 
