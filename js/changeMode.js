8d17c1a7a16ee41462e5ad8ede81804087b25bbc
Feedfoodie <admin@northbladetl.com>
2024-06-03 18:00:16 +0800

    navbar

--- js/changeMode.js
+++ js/changeMode.js
@@ -26,3 +26,23 @@ function changeWrapColor() {
 	  quotes[i].classList.toggle("day-mode-quotes")
 	}
 }
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} 
