4421b6003ebb7e2adfee5d7254ccef3d07ff1db9
Feedfoodie <admin@northbladetl.com>
2025-03-02 12:25:53 +0800

    Update changeMode.js

--- js/changeMode.js
+++ js/changeMode.js
@@ -1,6 +1,9 @@
 // Set Font Size Automatically
 function setFontSize(value) {
 	document.getElementById("content").style.fontSize = value;
+	document.getElementById("fast-comments").style.fontSize = value;
+	document.getElementById("username").style.fontSize = value;
+	document.getElementById("comment-text").style.fontSize = value;
 	var quote = document.getElementsByClassName("night-mode-quotes");
 		for (var i = 0; i < quote.length; i++) {
 			quote[i].style.fontSize = value;
@@ -48,10 +51,11 @@ function changeFontSize(change) {
 }
 
 //Change Color
+var quotes = 0;
 function changeWrapColor() {
 	document.getElementById("wrappertext").classList.toggle("day-mode");
 	document.getElementById("comment-area").classList.toggle("day-mode-comments");
-	var quotes = document.getElementsByClassName("night-mode-quotes");
+	quotes = document.getElementsByClassName("night-mode-quotes");
 	for (var i = 0; i < quotes.length; i++) {
 		quotes[i].classList.toggle("day-mode-quotes")
 	}
