916e42648aab13f4417e80118df04704f9f34158
Feedfoodie <admin@northbladetl.com>
2025-08-08 22:32:42 +0800

    Update Remove AggAnnoy.py

--- Remove AggAnnoy.py
+++ Remove AggAnnoy.py
@@ -33,7 +33,7 @@ for directory in directories_to_process:
 
                     # Conditionally remove the 'aggAnnoy' pattern if "ABSW" is not in the filename
                     #if "ABSW" not in entry.name:
-                    text = re.sub(r'SuandFriends.*?\n\n', '', text, flags=re.DOTALL)
+                        #text = re.sub(r'aggAnnoy.*?\n\n', '', text, flags=re.DOTALL)
 
                     # Apply all other static replacements from the dictionary
                     for old, new in replacements.items():
