515536a8b2023c6483ad0f620084420c5bc0320c
FeedFoodie <admin@northbladetl.com>
2026-02-12 18:43:52 +0800

    fixes

--- js/navBar.js
+++ js/navBar.js
@@ -1,1 +1,11 @@
-function navbarDropdown(id){document.getElementById(id).classList.toggle("show");}window.onclick=function(o){if(!o.target.matches(".dropbtn")){var n=document.getElementById("novelDropdown");n.classList.contains("show")&&n.classList.remove("show")}};
+function navbarDropdown(id) {
+  var dropdown = document.getElementById(id);
+  dropdown.classList.toggle("show");
+  var allOpen = document.querySelectorAll('.dropdown-content.show');
+  allOpen.forEach(function(el) {
+    if (el.id !== id) {
+      el.classList.remove('show');
+    }
+  });
+}
+window.onclick=function(o){if(!o.target.matches(".dropbtn")){var n=document.getElementById("novelDropdown");n.classList.contains("show")&&n.classList.remove("show")}};
